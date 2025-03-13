c00ad0f05c6bab6d3e09cf499bb2c246a97edd71
P-Adamo <adamopoulospan13@gmail.com>
2025-03-13 09:52:34 +0200

    Add files via upload

--- Full Scale Vehicle Simulation/plot_full_scale.m
+++ Full Scale Vehicle Simulation/plot_full_scale.m
@@ -28,12 +28,12 @@ t_axle=fs_ans(13,:);
 
 %% Vehicle Plot 
 car_graph=figure();
-set(car_graph,'position',[100,100,700,900]);
+set(car_graph,'position',[100,100,640,700]);
 car_graph_tiles=tiledlayout(2,1);
 car_graph_tiles.TileSpacing = 'compact';
 car_graph_tiles.Padding = 'compact';
-xlabel(car_graph_tiles,"Time (s)",fontsize=17);
-title(car_graph_tiles,"Full Scale Motor System During Deceleration",fontsize=17);
+xlabel(car_graph_tiles,"Χρόνος (s)",fontsize=14,FontName='Verdana');
+%title(car_graph_tiles,"Κινητήριο σύστημα κατά την πέδηση/ανάκτηση",fontsize=15,FontName='Verdana');
 
 tl1=nexttile;
 yyaxis left;
@@ -42,69 +42,56 @@ hold on;
 plot(tl1,time,car_speed,LineWidth=3,LineStyle="-");
 axis(tl1,[0 9 0 110]);
 yticks(0:10:110);
-ylabel("Linear Speed (km/h)",fontsize=14);
+ylabel("Ταχύτητα (km/h)",fontsize=14,FontName='Verdana');
+
 yyaxis right;
-plot(tl1,time,rotor_rpm,LineWidth=3);
+plot(tl1,[1 1],[-1000 3000],LineWidth=1,color='black');
+hold on;
+axis(tl1,[0 9 100 275]);
+yticks(0:25:550);
+plot(tl1,time,v_sc,LineWidth=3,Linestyle='--');
 grid on;
-axis(tl1,[0 9 0 2200]);
-ylabel("Rotational Speed (RPM)",fontsize=14);
-yticks(0:200:2200);
-plot(tl1,[6 6],[-1000 3000],LineWidth=1,color='black',linestyle='-');
-legend(tl1,'','Vehicle','Machine Rotor','',fontsize=14);
-title(tl1,'Speed',fontsize=17);
+plot(tl1,[6 6],[-1000 3000],LineWidth=1,color='black',Linestyle='-');
+legend(tl1,'','Όχημα','','Υπερπυκνωτές','',location='southeast',fontsize=12,FontName='Verdana');
+title(tl1,'Ταχύτητα / Τάση (DC Ζυγός=500V)',fontsize=15,FontName='Verdana');
 
 tl2=nexttile;
-plot(tl2,[1 1 ],[-1000 3000],LineWidth=1,color='black');
-hold on;
-plot(tl2,time,t_axle/1000,LineWidth=3,color="#0072BD");
-plot(tl2,time,t_elmag/1000,LineWidth=3,color='#D95319',LineStyle='--');
+plot(tl2,[1 1 ],[-10000 30000],LineWidth=1,color='black');
+hold on; 
+plot(tl2,time,t_axle,LineWidth=3,color="#0072BD");
+plot(tl2,time,t_elmag,LineWidth=3,color='#D95319',LineStyle='--');
 grid on;
-ylabel('Torque (kN*m)',fontsize=14);
-axis(tl2,[0 9 -2.5 0.500]);
-plot(tl2,[6 6],[-1000 3000],LineWidth=1,color='black');
-legend(tl2,'','Axle','Electromagnetic','',fontsize=14,location='north');
-title(tl2,'Torque',fontsize=17);
+ylabel('Ροπή (Nm)',fontsize=14,FontName='Verdana');
+axis(tl2,[0 9 -2300 250]);
+plot(tl2,[6 6],[-10000 30000],LineWidth=1,color='black');
+yticks(tl2,-2500:250:250);
+legend(tl2,'','Άξονας','Δρομέας','',fontsize=12,location='southeast',FontName='Verdana');
+title(tl2,'Ροπή',fontsize=15,FontName='Verdana');
 
 saveas(car_graph,'car_graph.svg');
 %% Current/Voltage Plot
 cur_vol_graph=figure();
-set(cur_vol_graph,'position',[0,-100,750,1400]);
-cur_vol_graph_tiles=tiledlayout(3,1);
+set(cur_vol_graph,'position',[0,-100,640,700]);
+cur_vol_graph_tiles=tiledlayout(2,1);
 cur_vol_graph_tiles.TileSpacing = 'compact';
 cur_vol_graph_tiles.Padding = 'compact';
-xlabel(cur_vol_graph_tiles,"Time (s)",fontsize=17);
-title(cur_vol_graph_tiles,"Full Scale Motor System During Deceleration",fontsize=17);
+xlabel(cur_vol_graph_tiles,"Χρόνος (s)",fontsize=14,FontName='Verdana');
+%title(cur_vol_graph_tiles,"Κινητήριο σύστημα κατά την πέδηση/ανάκτηση",fontsize=15,FontName='Verdana')
 
-tl1=nexttile;
-plot(tl1,[1 1],[-1000 3000],LineWidth=1,color='black');
-hold on;
-plot(tl1,time,v_dclink,LineWidth=3,LineStyle="-",color="#0072BD");
-axis(tl1,[0 9 0 525]);
-yticks(0:50:550);
-ylabel("Voltage (V)",fontsize=14);
-plot(tl1,time,v_arm,LineWidth=3,color='#D95319');
-plot(tl1,time,v_sc,LineWidth=3,color="#7E2F8E",Linestyle='--');
-grid on;
-plot(tl1,[6 6],[-1000 3000],LineWidth=1,color='black');
-legend(tl1,'','DC Link','Motor Armature','Supercapacitor','',fontsize=14,location='northeast');
-
-title(tl1,'Voltage',fontsize=17);
-
-
-tl2=nexttile(2,[2 1]);
+tl2=nexttile(1,[2 1]);
 plot(tl2,[1 1],[-10000 3000],LineWidth=1,color='black');
 hold on;
 plot(tl2,time,i_dclink,LineWidth=3,Linestyle='-',color="#0072BD");
-plot(tl2,time,i_sc,LineWidth=3,Linestyle='--',color='#7E2F8E');
+plot(tl2,time,i_sc/1.02,LineWidth=3,Linestyle='--',color='#7E2F8E');
 plot(tl2,time,i_bat,LineWidth=3,LineStyle="--",color="#77AC30");
 plot(tl2,time,i_conv,LineWidth=3,Linestyle='-',color="#4DBEEE");
 grid on;
-ylabel('Current (A)',fontsize=14);
-axis(tl2,[0 9 -850 50]);
+ylabel('Ρεύμα (A)',fontsize=14,FontName='Verdana');
+axis(tl2,[0 9 -800 50]);
 yticks(-1200:50:50);
 plot(tl2,[6 6],[-1000 3000],LineWidth=1,color='black');
-legend(tl2,'','DC Link','Supercapacitor','Battery','Converter','',fontsize=14,location='southeast');
-title(tl2,'Current',fontsize=17);
+legend(tl2,'','DC Ζυγός','Υπερπυκνωτές','Μπαταρία','Μετατροπέας','',fontsize=12,location='southeast',FontName='Verdana');
+title(tl2,'Ρεύμα',fontsize=15,FontName='Verdana');
 
 saveas(cur_vol_graph,'cur_vol_graph.svg');
 
--- Full Scale Vehicle Simulation/plot_full_scale_return.m
+++ Full Scale Vehicle Simulation/plot_full_scale_return.m
@@ -26,39 +26,41 @@ v_dclink=fs_ans(11,:);
 
 %% Current/Voltage Plot
 cur_vol_graph=figure();
-set(cur_vol_graph,'position',[0,-100,750,1400]);
-cur_vol_graph_tiles=tiledlayout(3,1);
+set(cur_vol_graph,'position',[0,-100,640,700]);
+cur_vol_graph_tiles=tiledlayout(4,1);
 cur_vol_graph_tiles.TileSpacing = 'compact';
 cur_vol_graph_tiles.Padding = 'compact';
-xlabel(cur_vol_graph_tiles,"Time (s)",fontsize=17);
-title(cur_vol_graph_tiles,"Full Scale Motor System During Energy Return",fontsize=17);
+xlabel(cur_vol_graph_tiles,"Χρόνος (s)",fontsize=14,FontName='Verdana');
+%title(cur_vol_graph_tiles,"Κινητήριο σύστημα κατά την επιστροφή ενέργειας",fontsize=17,FontName='Verdana')
 
 tl1=nexttile;
 plot(tl1,[1 1],[-1000 3000],LineWidth=1,color='black');
 yticks(0:50:550);
-ylabel("Voltage (V)",fontsize=14);
+ylabel("Τάση (V)",fontsize=14,FontName='Verdana');
 plot(tl1, [-1 -1],[-1 -1],LineWidth=3,LineStyle="-");
 hold on;
 plot(tl1,time,v_sc,LineWidth=3.5,Linestyle='-');
 grid on;
 xticks(tl1,0:1:25);
-axis(tl1,[0 12 0 170]);
-title(tl1,'Supercapacitor Voltage','DC Link Voltage = 500V',fontsize=17);
+axis(tl1,[0 12 95 165]);
+yticks(tl1,0:10:200);
+ylabel('Τάση (V)',fontsize=14,FontName='Verdana');
+title(tl1,'Τάση Υπερπυκνωτών (DC Ζυγός=500V)',fontsize=15,FontName='Verdana');
 
 %%
-tl2=nexttile(2,[2 1]);
+tl2=nexttile(2,[3 1]);
 plot(tl2,[1 1],[-10000 3000],LineWidth=1,color='black');
 hold on;
-plot(tl2,time,i_dclink,LineWidth=3.5,Linestyle='--',color="#0072BD");
+plot(tl2,time,i_dclink,LineWidth=4,Linestyle='--',color="#0072BD");
 plot(tl2,time,i_sc,LineWidth=3,Linestyle='--',color='#7E2F8E');
 plot(tl2,time,i_bat,LineWidth=3,LineStyle="-",color="#77AC30");
 plot(tl2,time,i_conv,LineWidth=3,Linestyle='-',color="#4DBEEE");
 grid on;
-ylabel('Current (A)',fontsize=14);
+ylabel('Ρεύμα (A)',fontsize=14,FontName='Verdana');
 axis(tl2,[0 12 0 155]);
 yticks(0:10:200);
-legend(tl2,'','DC Link','Supercapacitor','Battery','Converter',fontsize=14,location='northwest');
-title(tl2,'Current',fontsize=17);
+legend(tl2,'','DC Ζυγός','Υπερπυκνωτές','Μπαταρία','Μετατροπέας',fontsize=12,location='northwest',FontName='Verdana');
+title(tl2,'Ρεύμα',fontsize=15,FontName='Verdana');
 xticks(tl2,0:1:25);
 
 saveas(cur_vol_graph,'cur_vol_graph_return.svg');
